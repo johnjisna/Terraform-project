a3d0fef78ff035b6614d5645bb35af2cac2b0e1a
Jisna C John <109332601+johnjisna@users.noreply.github.com>
2025-02-17 14:51:07 +0530

    Update outputs.tf

--- ecr/outputs.tf
+++ ecr/outputs.tf
@@ -2,3 +2,12 @@ output "repository_url" {
   description = "The URL of the ECR repository"
   value       = aws_ecr_repository.this.repository_url
 }
+
+output "repository_name" {
+  description = "The URL of the ECR repository"
+  value       = aws_ecr_repository.this.name
+}
+
+output "repository_arn" {
+  value = aws_ecr_repository.this.arn
+}
