a21540cdf68a4f817573f62122e9e54734c65945
Jisna C John <109332601+johnjisna@users.noreply.github.com>
2025-02-17 14:52:31 +0530

    Update variables.tf

--- iam/variables.tf
+++ iam/variables.tf
@@ -1,26 +1,39 @@
 variable "iam_user_name" {
-  description = "IAM user name"
+  description = "The name of the IAM user"
   type        = string
 }
 
 variable "iam_role_name" {
-  description = "IAM role name"
+  description = "The name of the IAM role"
   type        = string
 }
 
 variable "trusted_services" {
-  description = "List of AWS services that can assume this role"
+  description = "List of services trusted to assume the IAM role"
   type        = list(string)
-  default     = []
 }
 
 variable "create_role" {
-  description = "Flag to create IAM role (true for backend, false for frontend)"
+  description = "Flag to determine if IAM role should be created"
   type        = bool
-  default     = false
 }
 
 variable "iam_policies" {
-  description = "Map of IAM policy names to file paths"
+  description = "Map of policy names to policy file paths"
+  type        = map(string)
+}
+
+variable "resource_arn_mapping" {
+  description = "Mapping of policy names to the corresponding resource ARNs"
+  type        = map(string)
+}
+
+variable "user_policy_mapping" {
+  description = "Mapping of policies to attach to the IAM user"
+  type        = map(string)
+}
+
+variable "role_policy_mapping" {
+  description = "Mapping of policies to attach to the IAM role"
   type        = map(string)
 }
