8ed3083cc261464dd3ad9b08a30be89213273192
Jisna C John <109332601+johnjisna@users.noreply.github.com>
2025-02-17 14:52:11 +0530

    Update outputs.tf

--- iam/outputs.tf
+++ iam/outputs.tf
@@ -12,3 +12,8 @@ output "iam_policies" {
   description = "IAM policies created"
   value       = keys(aws_iam_policy.policies)
 }
+
+output "iam_user_arn" {
+  description = "IAM User ARN"
+  value       = aws_iam_user.user.arn
+}
