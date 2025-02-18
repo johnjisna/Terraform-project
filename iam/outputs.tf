9d2de27961a93663f91fc08439cdad024302af2f
Jisna C John <109332601+johnjisna@users.noreply.github.com>
2025-02-18 10:32:24 +0530

    Update outputs.tf

--- iam/outputs.tf
+++ iam/outputs.tf
@@ -3,11 +3,6 @@ output "iam_user_name" {
   value       = aws_iam_user.user.name
 }
 
-output "iam_role_name" {
-  description = "IAM role name (if created)"
-  value       = var.create_role ? aws_iam_role.role[0].name : null
-}
-
 output "iam_policies" {
   description = "IAM policies created"
   value       = keys(aws_iam_policy.policies)
