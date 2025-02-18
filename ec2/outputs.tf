213f095f048fbbd5e5ab33f57e36be27ab78f967
Jisna C John <109332601+johnjisna@users.noreply.github.com>
2025-02-18 14:04:30 +0530

    Update outputs.tf

--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -7,7 +7,3 @@ output "public_ip" {
   description = "Public IP address of the instance"
   value       = aws_instance.web.public_ip
 }
-
-output "latest_ubuntu_ami_id" {
-  value = data.aws_ami.ubuntu.id
-}
