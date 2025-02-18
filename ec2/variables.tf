a52564e5774d07cbd22331b5f31af8a62a4c7272
Jisna C John <109332601+johnjisna@users.noreply.github.com>
2025-02-18 14:05:06 +0530

    Update variables.tf

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,40 +1,34 @@
 variable "instance_type" {
-  description = "Instance type"
+  description = "EC2 instance type."
   type        = string
+  default     = "t2.micro"
 }
 
 variable "key_name" {
-  description = "Key pair name for SSH access"
+  description = "Key pair name for the EC2 instance."
   type        = string
 }
 
 variable "instance_name" {
-  description = "Name of the instance"
-  type        = string
-}
-
-variable "secret_id" {
-  description = "ID of the secret to fetch"
-  type        = string
-}
-
-variable "region" {
-  description = "AWS region"
-  type        = string
-}
-
-variable "ecr_repository_url" {
-  description = "The URL of the ECR repository"
+  description = "Name tag for the EC2 instance."
   type        = string
 }
 
 variable "environment" {
-  description = "Environment name (e.g., dev, staging, prod)"
+  description = "Environment tag for the EC2 instance."
   type        = string
 }
 
-variable "EC2_AMI_FILTER" {
+
+variable "preferred_filter" {
+  description = "AMI filter for the preferred Ubuntu version (e.g. Ubuntu 24)."
   type        = string
-  default     = "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"
-  description = "AMI name to be used to create the EC2 instance"
+  default     = "ubuntu/images/hvm-ssd/ubuntu-24*-amd64-server-*"
+}
+
+
+variable "fallback_filter" {
+  description = "AMI filter for fallback Ubuntu images."
+  type        = string
+  default     = "ubuntu/images/hvm-ssd/ubuntu-*-amd64-server-*"
 }
